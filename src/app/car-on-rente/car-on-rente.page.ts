5bcb3ba0adf568f83b69456d5ebeda32bb419fbc
Piyush8456 <p8use9@gmail.com>
2023-11-28 18:26:38 +0530

    new page

--- src/app/car-on-rente/car-on-rente.page.ts
+++ src/app/car-on-rente/car-on-rente.page.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-car-on-rente',
   templateUrl: './car-on-rente.page.html',
   styleUrls: ['./car-on-rente.page.scss'],
 })
 export class CarOnRentePage  {
+  dateExample = new Date().toISOString();
+  endDete = new Date().toDateString();
 
   constructor(private router : Router ) { }
 
   backtoHome(){
     this.router.navigate(['/'])
   }
-
 }
